dad256a64e13ee1c5a93044ba9f9dcfa52bf7d82
hd <hdshare@vip.qq.com>
2025-08-04 02:07:37 +0800

    chore: use bsh demo

--- app/src/main/java/me/hd/beanshell_android/MainActivity.kt
+++ app/src/main/java/me/hd/beanshell_android/MainActivity.kt
@@ -1,11 +1,37 @@
 package me.hd.beanshell_android
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import bsh.BshMethod
+import bsh.Interpreter
+import me.hd.beanshell_android.databinding.ActivityMainBinding
 
+@SuppressLint("SetTextI18n")
 class MainActivity : Activity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    fun log(str: String) {
+        binding.tvLog.text = "[${System.currentTimeMillis()}] $str"
+    }
+
+    private fun initView() {
+        binding.btnRun.setOnClickListener {
+            runCatching {
+                Interpreter().apply {
+                    nameSpace.setMethod(BshMethod(MainActivity::class.java.getMethod("log", String::class.java), this@MainActivity))
+                }.eval(binding.edtCode.text.toString())
+            }.onFailure { e ->
+                Log.e("BeanShell", "Run Failed", e)
+            }
+        }
     }
 }
