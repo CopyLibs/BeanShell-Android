342454d721e2cb76b4a2a84ad2375db01c64acbe
hd <hdshare@vip.qq.com>
2025-09-03 13:44:28 +0800

    chore: add publish maven

--- bsh-300-eee36c8/build.gradle.kts
+++ bsh-300-eee36c8/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.34.0"
+    id("signing")
 }
 
 android {
@@ -22,4 +26,39 @@ android {
 
 dependencies {
     implementation(libs.dalvik.dx)
+}
+
+mavenPublishing {
+    configure(AndroidSingleVariantLibrary(publishJavadocJar = false))
+    coordinates(
+        groupId = "io.github.copylibs",
+        artifactId = "beanshell-android",
+        version = "3.0.0.beta1"
+    )
+
+    pom {
+        name = "BeanShell-Android"
+        description = "BeanShell Support For Android"
+        url = "https://github.com/CopyLibs/BeanShell-Android"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                name = "HdShare"
+                url = "https://github.com/HdShare"
+            }
+        }
+        scm {
+            url = "https://github.com/CopyLibs/BeanShell-Android"
+            connection = "scm:git:git://github.com/CopyLibs/BeanShell-Android.git"
+            developerConnection = "scm:git:ssh://git@github.com/CopyLibs/BeanShell-Android.git"
+        }
+    }
+
+    publishToMavenCentral()
+    signAllPublications()
 }
