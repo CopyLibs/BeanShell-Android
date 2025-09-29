a2eeb0ec942ee69d76e42964d6ee83e443817c7d
hd <hdshare@vip.qq.com>
2025-09-29 20:01:40 +0800

    chore: bump 3.0.0.beta3

--- bsh-300-eee36c8/build.gradle.kts
+++ bsh-300-eee36c8/build.gradle.kts
@@ -33,7 +33,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.copylibs",
         artifactId = "beanshell-android",
-        version = "3.0.0.beta2"
+        version = "3.0.0.beta3"
     )
 
     pom {
