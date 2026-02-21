a841c6907adaf8eaec937ff4fc0a1958c7bb1200
hd <hdshare@vip.qq.com>
2026-02-21 15:51:06 +0800

    feat: getLoaderByJar

--- app/src/main/java/io/github/copylibs/bsh/plugin/Plugin.kt
+++ app/src/main/java/io/github/copylibs/bsh/plugin/Plugin.kt
@@ -8,52 +8,67 @@ class Plugin {
     private val interpreter = Interpreter()
 
     /**
-     * 导入类
+     * 导入 Class
      */
     fun importClass(className: String) {
         interpreter.nameSpace.importClass(className)
     }
 
     /**
-     * 导入包
+     * 导入 Package
      */
     fun importPackage(packageName: String) {
         interpreter.nameSpace.importPackage(packageName)
     }
 
     /**
-     * 设置变量
+     * 设置 Variable
      */
     fun setVariable(name: String, value: Any) {
         interpreter.set(name, value)
     }
 
     /**
-     * 设置方法
+     * 设置 Method
      */
     fun setMethod(method: BshMethod) {
         interpreter.nameSpace.setMethod(method)
     }
 
     /**
-     * 执行代码
+     * 执行 Code
      */
     fun eval(code: String) {
         interpreter.eval(code)
     }
 
     /**
-     * 执行文件
+     * 执行 Path
      */
     fun source(path: String) {
         interpreter.source(path)
     }
 
     /**
-     * 加载Dex
+     * 添加 ClassLoader
+     */
+    fun addClassLoader(loader: ClassLoader) {
+        interpreter.addClassLoader(loader)
+    }
+
+    /**
+     * 加载 Dex
      */
     fun loadDex(path: String) {
         val clsLoader = BshLoaderHelper.getLoaderByDex(path, Plugin::class.java.classLoader)
-        interpreter.addClassLoader(clsLoader)
+        addClassLoader(clsLoader)
+    }
+
+    /**
+     * 加载 Jar
+     */
+    fun loadJar(path: String) {
+        val clsLoader = BshLoaderHelper.getLoaderByJar(path, Plugin::class.java.classLoader)
+        addClassLoader(clsLoader)
     }
 }
