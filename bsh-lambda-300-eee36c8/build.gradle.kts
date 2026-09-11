8b51f55f2fcc2f506d1d5bdb1d84a4703f283be1
hd <hdshare@vip.qq.com>
2026-09-11 20:16:18 +0800

    chore: bump 3.0.0.beta13

--- bsh-lambda-300-eee36c8/build.gradle.kts
+++ bsh-lambda-300-eee36c8/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 val publishGroupId = "io.github.copylibs"
 val publishArtifactId = "beanshell-android-lambda"
-val publishVersion = "3.0.0.beta12"
+val publishVersion = "3.0.0.beta13"
 
 android {
     namespace = "bsh.lambda_300_eee36c8"
